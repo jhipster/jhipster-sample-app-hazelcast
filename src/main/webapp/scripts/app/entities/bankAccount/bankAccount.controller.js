2166577e7135a6794f92857aedadf48bfb6ba8ff
Julien Dubois <julien.dubois@gmail.com>
2015-11-04 17:38:43 +0100

    automatic project update

--- src/main/webapp/scripts/app/entities/bankAccount/bankAccount.controller.js
+++ src/main/webapp/scripts/app/entities/bankAccount/bankAccount.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('sampleHazelcastApp')
     .controller('BankAccountController', function ($scope, $state, $modal, BankAccount) {
+      
         $scope.bankAccounts = [];
         $scope.loadAll = function() {
             BankAccount.query(function(result) {
--- src/main/webapp/scripts/app/entities/label/label.controller.js
+++ src/main/webapp/scripts/app/entities/label/label.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('sampleHazelcastApp')
     .controller('LabelController', function ($scope, $state, $modal, Label) {
+      
         $scope.labels = [];
         $scope.loadAll = function() {
             Label.query(function(result) {
