f5365a10e957253e63f64f4f187adacd8d345028
Julien Dubois <julien.dubois@gmail.com>
2015-11-20 16:10:29 +0100

    automatic project update

--- src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
+++ src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
@@ -49,7 +49,7 @@ angular.module('sampleHazelcastApp')
                     size: 'lg',
                     resolve: {
                         threadDump: function() {
-                            return data;
+                            return data.content;
                         }
 
                     }
