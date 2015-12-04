be5f316a84c25ae605cba26f8ea0c4516efb5e08
Julien Dubois <julien.dubois@gmail.com>
2015-12-04 15:18:09 +0100

    automatic project update

---
+++ src/main/webapp/scripts/components/form/uib-pager.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('sampleHazelcastApp')
+    .config(function (uibPagerConfig) {
+        uibPagerConfig.itemsPerPage = 20;
+        uibPagerConfig.previousText = '«';
+        uibPagerConfig.nextText = '»';
+    });
--- src/main/webapp/scripts/components/user/user.service.js
+++ src/main/webapp/scripts/components/user/user.service.js
@@ -11,6 +11,8 @@ angular.module('sampleHazelcastApp')
                         return data;
                     }
                 },
-                'update': { method:'PUT' }
+                'save': { method:'POST' },
+                'update': { method:'PUT' },
+                'delete':{ method:'DELETE'}
             });
         });
