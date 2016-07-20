6400366bb75ae526b9ad63aec1031d63ae96de75
Julien Dubois <julien.dubois@gmail.com>
2016-07-21 00:34:15 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/config/JHipsterProperties.java
+++ src/main/java/io/github/jhipster/sample/config/JHipsterProperties.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.cors.CorsConfiguration;
 
-
 /**
  * Properties specific to JHipster.
  *
@@ -212,8 +211,6 @@ public class JHipsterProperties {
 
         private String licenseUrl;
 
-        private Boolean enabled;
-
         public String getTitle() {
             return title;
         }
@@ -285,14 +282,6 @@ public class JHipsterProperties {
         public void setLicenseUrl(String licenseUrl) {
             this.licenseUrl = licenseUrl;
         }
-
-        public Boolean isEnabled() {
-            return enabled;
-        }
-
-        public void setEnabled(Boolean enabled) {
-            this.enabled = enabled;
-        }
     }
 
     public static class Metrics {
@@ -320,7 +309,6 @@ public class JHipsterProperties {
         public Logs getLogs() {
             return logs;
         }
-
 
         public static class Jmx {
 
@@ -474,6 +462,7 @@ public class JHipsterProperties {
 
     }
 
+
     public static class Ribbon {
 
         private String[] displayOnActiveProfiles;
@@ -481,10 +470,9 @@ public class JHipsterProperties {
         public String[] getDisplayOnActiveProfiles() {
             return displayOnActiveProfiles;
         }
-        
+
         public void setDisplayOnActiveProfiles(String[] displayOnActiveProfiles) {
             this.displayOnActiveProfiles = displayOnActiveProfiles;
         }
-    }  
-
+    }
 }
--- src/main/webapp/app/entities/label/label-detail.controller.js
+++ src/main/webapp/app/entities/label/label-detail.controller.js
@@ -5,12 +5,13 @@
         .module('jhipsterHazelcastSampleApplicationApp')
         .controller('LabelDetailController', LabelDetailController);
 
-    LabelDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'entity', 'Label', 'Operation'];
+    LabelDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Label', 'Operation'];
 
-    function LabelDetailController($scope, $rootScope, $stateParams, entity, Label, Operation) {
+    function LabelDetailController($scope, $rootScope, $stateParams, previousState, entity, Label, Operation) {
         var vm = this;
 
         vm.label = entity;
+        vm.previousState = previousState.name;
 
         var unsubscribe = $rootScope.$on('jhipsterHazelcastSampleApplicationApp:labelUpdate', function(event, result) {
             vm.label = result;
