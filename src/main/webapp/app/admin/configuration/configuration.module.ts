fa8897a4a147953d7eb095e3fc319f656c36b0b5
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-12-22 08:57:26 +0100

    automatic project update

--- src/main/webapp/app/admin/configuration/configuration.module.ts
+++ src/main/webapp/app/admin/configuration/configuration.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { JhipsterHazelcastSampleApplicationSharedModule } from 'app/shared/shared.module';
 
-import { JhiConfigurationComponent } from './configuration.component';
+import { ConfigurationComponent } from './configuration.component';
 
 import { configurationRoute } from './configuration.route';
 
 @NgModule({
   imports: [JhipsterHazelcastSampleApplicationSharedModule, RouterModule.forChild([configurationRoute])],
-  declarations: [JhiConfigurationComponent]
+  declarations: [ConfigurationComponent]
 })
 export class ConfigurationModule {}
--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -8,7 +8,7 @@ import { JhipsterHazelcastSampleApplicationAppRoutingModule } from './app-routin
 import { JhipsterHazelcastSampleApplicationHomeModule } from './home/home.module';
 import { JhipsterHazelcastSampleApplicationEntityModule } from './entities/entity.module';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
-import { JhiMainComponent } from './layouts/main/main.component';
+import { MainComponent } from './layouts/main/main.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
@@ -25,7 +25,7 @@ import { ErrorComponent } from './layouts/error/error.component';
     JhipsterHazelcastSampleApplicationEntityModule,
     JhipsterHazelcastSampleApplicationAppRoutingModule
   ],
-  declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
-  bootstrap: [JhiMainComponent]
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  bootstrap: [MainComponent]
 })
 export class JhipsterHazelcastSampleApplicationAppModule {}
